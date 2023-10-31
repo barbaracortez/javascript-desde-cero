71d4a489b0e686b8f6a6af1e7be70e518c4c5629
Barbara Cortez <cortezbarbara.dev@gmail.com>
2023-10-30 22:29:57 -0300

    terminando ejercicios 3 condicionales

--- 03.Condicionales/ejercicios03.js
+++ 03.Condicionales/ejercicios03.js
@@ -1,96 +1,158 @@
-function mayorEdad(personas){
-    //Crea una condición que comprueba si las personas pasadas por parámetros son mayores de edad
-    //Si son mayores de edad, tienes que retornar "Es mayor de edad", en caso que no cumpla la condición 
-    //tendrás que retornar "Es menor de edad"
-    //Tu código:👇
-
+function mayorEdad(personas) {
+  //Crea una condición que comprueba si las personas pasadas por parámetros son mayores de edad
+  //Si son mayores de edad, tienes que retornar "Es mayor de edad", en caso que no cumpla la condición
+  //tendrás que retornar "Es menor de edad"
+  //Tu código:👇
+  if (personas >= 18) {
+    return "Es mayor de edad";
+  } else {
+    return "Es menor de edad";
+  }
 }
 
-function salario(personal, exp){
-    // En una empresa se le paga al personal un bono según su la experiencia que llevan en la empresa
-    // si la persona lleva menor o igual a 5 años el bono es de $5.000, mayor a 5 años y menor a 20 años es de $10.000
-    // mayor o igual a 20 años, el bono es de $15.000. tendrás que retornar un mensaje con el nombre del personal y el bono correspondiente
-    // Ejemplo: "Pedro tu bono es de $10.000"
-    // Tu código:👇
-    
+function salario(personal, exp) {
+  // En una empresa se le paga al personal un bono según su la experiencia que llevan en la empresa
+  // si la persona lleva menor o igual a 5 años el bono es de $5.000, mayor a 5 años y menor a 20 años es de $10.000
+  // mayor o igual a 20 años, el bono es de $15.000. tendrás que retornar un mensaje con el nombre del personal y el bono correspondiente
+  // Ejemplo: "Pedro tu bono es de $10.000"
+  // Tu código:👇
+  let bono;
+
+  if (exp <= 5) {
+    bono = "$5.000";
+  } else if (exp > 5 && exp < 20) {
+    bono = "$10.000";
+  } else {
+    bono = "$15.000";
+  }
+
+  return `${personal} tu bono es de ${bono}`;
 }
 
-function numPar(num){
-    // Retorna true si 'num' es un numero par, casi contrario retorna false
-    // Tu código:👇
-
+function numPar(num) {
+  // Retorna true si 'num' es un numero par, casi contrario retorna false
+  // Tu código:👇
+  if (num% 2 ===0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-function numImpar(num){
-    // Retorna true si 'num' es un numero impar, casi contrario retorna false
-    // Tu código:👇
-
+function numImpar(num) {
+  // Retorna true si 'num' es un numero impar, casi contrario retorna false
+  // Tu código:👇
+  return num % 2 != 0;
 }
 
-function numPositivoNegativo(num){
-    // Tendrás que retornar un mensaje según si el numero es positivo o negativo
-    // Positivos -> "El numero es positivo"
-    // Negativo -> "El numero es negativo"
-    // Tu código:👇
-
+function numPositivoNegativo(num) {
+  // Tendrás que retornar un mensaje según si el numero es positivo o negativo
+  // Positivos -> "El numero es positivo"
+  // Negativo -> "El numero es negativo"
+  // Tu código:👇
+  if (num > 0) {
+    return "El numero es positivo"
+  }else if (num < 0) {
+    return "El numero es negativo"
+  } else {
+    return "El numero es cero"
+  }
 }
 
-function cambioPesos(monto, pago){
-    // Una empresa recibe sus pagos en monedas extranjeras tanto dólares como euros. Tendrás hacer un programa
-    // que si el pago fue en "dólares" o "euros" convertir a pesos argentinos y retornar el resultado.
-    //  1 USD = $375  y 1 EUR = $383 . Si el pago fue en 'pesos argentinos', retornar el precio tal cual
-    // Tu código:👇
-    
+function cambioPesos(monto, pago) {
+  // Una empresa recibe sus pagos en monedas extranjeras tanto dólares como euros. Tendrás hacer un programa
+  // que si el pago fue en "dólares" o "euros" convertir a pesos argentinos y retornar el resultado.
+  //  1 USD = $375  y 1 EUR = $383 . Si el pago fue en 'pesos argentinos', retornar el precio tal cual
+  // Tu código:EUR
+  const USD_TO_ARS = 375;
+  const EUR_TO_ARS = 383;
+
+  if (pago === "dólares") {
+    return monto * USD_TO_ARS;
+  } else if (pago === "euros") {
+    return monto * EUR_TO_ARS;
+  } else if (pago === "pesos argentinos") {
+    return monto;
+  } else {
+    return "Moneda no reconocida";
+  }
 }
 
-function climaActual(clima){
-    //Haz un programa el cual te diga que te recomienda hacer según el clima actual! en base a la siguiente información:
-    // "Soleado" -> "El día está soleado puedes salir tranquilo!"
-    // "Lluvioso" -> "Está lloviendo, sal con un impermeable!"
-    // "Frio" -> "Si sales sin abrigo, te congelas!"
-    // "Ventoso" -> "Hay mucho viento, sería bueno un rompe viento!"
-    // Tu código:👇
 
+function climaActual(clima) {
+  //Haz un programa el cual te diga que te recomienda hacer según el clima actual! en base a la siguiente información:
+  // "Soleado" -> "El día está soleado puedes salir tranquilo!"
+  // "Lluvioso" -> "Está lloviendo, sal con un impermeable!"
+  // "Frio" -> "Si sales sin abrigo, te congelas!"
+  // "Ventoso" -> "Hay mucho viento, sería bueno un rompe viento!"
+  // Tu código:👇
+    if (clima === "Soleado") {
+      return "El día está soleado puedes salir tranquilo!";
+    } else if (clima === "Lluvioso") {
+      return "Está lloviendo, sal con un impermeable!";
+    } else if (clima === "Frio") {
+      return "Si sales sin abrigo, te congelas!";
+    } else if (clima === "Ventoso") {
+      return "Hay mucho viento, sería bueno un rompe viento!";
+    } else {
+      return "Clima no reconocido";
+    }
+  }
+
+
+
+
+function vocales(char) {
+  // Se pasaran varias letras por parámetros, tendrás que comprobar que sea una vocal y retornar el siguiente
+  // mensaje "Es vocal"
+  // Tu código:👇
+  const VOCALS = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+  if (VOCALS.includes(char)) {
+    return "Es vocal";
+  } else {
+    return false;
+  }
 }
 
-function vocales(char){
-    // Se pasaran varias letras por parámetros, tendrás que comprobar que sea una vocal y retornar el siguiente
-    // mensaje "Es vocal"
-    // Tu código:👇
-    
+function nValor(juan) {
+  //Juan tiene N dólares, David tiene la mitad de lo que posee Juan y José la mitad de lo que poseen Juan y David juntos.
+  //Retornar el promedio de la cantidad de dinero que tienen entre los tres y redondear hacia arriba
+  //Tu código:👇
+  const david = juan / 2;
+  const jose = (juan + david) / 2;
+  const promedio = (juan + david + jose)/ 3;
+  return Math.ceil(promedio);
 }
 
-function nValor(juan){
-    //Juan tiene N dólares, David tiene la mitad de lo que posee Juan y José la mitad de lo que poseen Juan y David juntos.
-    //Retornar el promedio de la cantidad de dinero que tienen entre los tres y redondear hacia arriba
-    //Tu código:👇
-
+function numMúltiplo(num1, num2) {
+  //Comprueba si num1 y num2 son múltiplos entre si, en caso de serlos retorna "Son múltiplos",
+  //caso contrario retornar "No son múltiplos"
+  //Tu código:👇
+  if (num1 % num2 === 0 || num2 % num1 === 0){
+    return "Son múltiplos";
+  } else {
+    return "No son múltiplos"
+  }
 }
 
-function numMúltiplo(num1, num2){
-    //Comprueba si num1 y num2 son múltiplos entre si, en caso de serlos retorna "Son múltiplos",
-    //caso contrario retornar "No son múltiplos"
-    //Tu código:👇
-
-}
-
-function length(str){
-    // Comprueba si 'str' tiene un longitud mayor que 5, de ser así tienes que retornar true
-    // caso contrario retornar false.
-    // Tu código:👇
-
+function length(str) {
+  // Comprueba si 'str' tiene un longitud mayor que 5, de ser así tienes que retornar true
+  // caso contrario retornar false.
+  // Tu código:👇
+  return str.length > 5;
 }
 
 module.exports = {
-    mayorEdad,
-    salario,
-    numPar,
-    numImpar,
-    numPositivoNegativo,
-    cambioPesos,
-    climaActual,
-    vocales,
-    nValor,
-    numMúltiplo,
-    length
-}
+  mayorEdad,
+  salario,
+  numPar,
+  numImpar,
+  numPositivoNegativo,
+  cambioPesos,
+  climaActual,
+  vocales,
+  nValor,
+  numMúltiplo,
+  length,
+};
